a3e1d1e9b6cf1acddd642cc9240cb268fd60eaaa
SurianBr <rmsurian@hotmail.com>
2021-04-28 20:22:49 -0300

    Adicionando ordens para a nave

--- scripts/Ship.js
+++ scripts/Ship.js
@@ -1,7 +1,22 @@
 class Ship {
     constructor(phaser, x, y, texture, frame, config){
-        // adiciona ship ni stage
+        // adiciona ship no mundo
         this.objeto = phaser.matter.add.sprite(x, y, texture, frame, config);
-        this.estado = NAVE_OCIOSA
+
+        // Estado atual da nave
+        this.estado = NAVE_OCIOSA;
+        
+        // Ordens da nave
+        this.ordens = [];
+    }
+
+    // Atualiza estado atual da nave
+    updade() {
+        switch(this.estado){
+            case NAVE_OCIOSA:
+                if (ordens.length > 0)
+                    this.estado = NAVE_APONTANDO_PARA_DESTINO;
+                break;
+        }
     }
   }
--- scripts/constantes.js
+++ scripts/constantes.js
@@ -1,1 +1,6 @@
+// Estados de uma nave
 const NAVE_OCIOSA = 0;
+const NAVE_APONTANDO_PARA_DESTINO = 0;
+
+// Tipos de ordens
+const ORDEM_MOVIMENTACAO = 0;
